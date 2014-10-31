4181aba98685fbd6bc052cd6837e99523fe6930b
wagner <wagner@king-nb.(none)>
2014-10-31 18:24:59 -0200

    pulsar 0.2

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import sys
-import json
-import base64
-import re
-import urllib
-import urllib2
-import time
 import os
-import bencode
+import urllib2
 import hashlib
-inicio = time.time()
-cache_age = 48 * 60 * 60
+import time
+import re
+import xbmcaddon
+import xbmcplugin
+from pulsar import provider
+import shelve
+import thread
 
-URL_BASE = 'http://www.storetorrent.org/s/'
+begin = time.time()
+__addon__ = xbmcaddon.Addon(str(sys.argv[0]))
+addon_dir = xbmc.translatePath(__addon__.getAddonInfo('path'))
+sys.path.append(os.path.join(addon_dir, 'resources', 'lib' ))
+inicio = time.time()
+
+URL_BASE = 'https://btdigg.org/search'
+
+PREFIX_LOG = 'ANIMESBR - '
+HEADERS = { 'Referer' : base_url,
+            'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
+}
+
+cache_prefix = xbmc.translatePath('special://temp') + __addon__.getAddonInfo('name').lower().replace(' ','_') + '_cache_'
 
 def search(query):
     return []
@@ -19,39 +31,42 @@ def search(query):
 def search_movie(imdb_id, name, year):
     return []
 
-def search_episode(imdb_id, tvdb_id, name, season, episode):
-    print 'ANIMES-BR Provider Seaching for: ' + name + ' (' + str(season).zfill(2) + 'E' + str(episode).zfill(2) + ')'
+def search_episode(ep):
+    name = ep['title']
+    season = ep['season']
+    episode = ep['episode']
+    tvdb_id = ep['tvdb_id']
+    print PREFIX_LOG + 'Seaching for: ' + name + ' (S' + str(season).zfill(2) + 'E' + str(episode).zfill(2) + ')'
     result = []
     if(tvdb_id == '79824'):
         result = search_naruto_shippuden(season, episode)
     elif(tvdb_id == '81797'):
         result = search_one_piece(season, episode)
-    print 'ANIMES-BR Provider Result:' + str(result)
+    print PREFIX_LOG + 'Result:' + str(result)
     return result
 
 def search_naruto_shippuden(season, episode):
-    result = []
-    data = get_cached_url('http://en.wikipedia.org/wiki/List_of_Naruto:_Shippuden_episodes')
-    season_episode_fix = [{"season": season, "first_episode": first_episode } for season, first_episode in re.findall(r'>(?:Season ([0-9][0-9]*))?:.*?>([0-9]*)<\/th>', data, re.DOTALL)]
+    season_episode_fix = get_cached_func('get_shippuden_fix')
     episode_number = []
     for item in  season_episode_fix:
         if(item['season'] == season):
             episode_number = int(item['first_episode']) + (episode - 1)
-            result.append({"uri": storetorrent_get_magnet('narutoPROJECT Shippu' + str(episode_number))})
+            resp = provider.GET(URL_BASE, params={"q": 'narutoPROJECT Shippuuden ' + str(episode_number),})
+	        return provider.extract_magnets(resp.data)
             break
-    return result
-
+    return []
+    
 def search_one_piece(season, episode):
     result = []
-    data = get_cached_url('http://en.wikipedia.org/wiki/List_of_One_Piece_episodes')
-    season_episode_fix = [{"season": season, "first_episode": first_episode } for season, first_episode in re.findall(r'>(?:Season ([0-9][0-9]*))\ ?\(.*?\).*?(?:>([0-9][0-9]*)</th>)', data, re.DOTALL)]
+    season_episode_fix = get_cached_func('get_onepiece_fix')
     episode_number = []
     for item in  season_episode_fix:
         if(item['season'] == season):
             episode_number = int(item['first_episode']) + (episode - 1)
-            result.append({"uri": storetorrent_get_magnet('piecePROJECT ' + str(episode_number) + 'HD')})
+            resp = provider.GET(URL_BASE, params={"q": 'piecePROJECT ' + str(episode_number) + 'HD',})
+	        return provider.extract_magnets(resp.data)
             break
-    return result
+    return []
 
 def storetorrent_get_magnet(query):
     req = urllib2.Request(URL_BASE + urllib.quote_plus(query))
@@ -59,47 +74,52 @@ def storetorrent_get_magnet(query):
     for magnet in re.findall(r'(magnet:.*)" class', data):
         return magnet
 
-def torrent2mag(torrent_url):
-    response = urllib2.urlopen(torrent_url)
-    torrent = response.read()
-    metadata = bencode.bdecode(torrent)
-    hashcontents = bencode.bencode(metadata['info'])
-    digest = hashlib.sha1(hashcontents).digest()
-    b32hash = base64.b32encode(digest)
-    magneturl = 'magnet:?xt=urn:btih:' + b32hash  + '&dn=' + metadata['info']['name']
-    return magneturl
+def get_onepiece_fix():
+    data = get_url('http://en.wikipedia.org/wiki/List_of_One_Piece_episodes')
+    return [{"season": season, "first_episode": first_episode } for season, first_episode in re.findall(r'>(?:Season ([0-9][0-9]*))\ ?\(.*?\).*?(?:>([0-9][0-9]*)</th>)', data, re.DOTALL)]
 
-def get_cached_url(url):
-    data = ''
-    m = hashlib.md5()
-    m.update(url)
-    url_hash = m.hexdigest()
-    cache_file = xbmc.translatePath('special://temp') + url_hash
-    print 'ANIMES-BR Provider:  cache -> ' + cache_file
-    if(os.path.isfile(cache_file)):
-        if ((time.time() - os.stat(cache_file).st_mtime)  > cache_age):
-            print 'ANIMES-BR Provider: Invalid cache!'
-            req = urllib2.Request(url)
-            data = urllib2.urlopen(req).read()
-            f = open(cache_file, "w")
-            f.write(data)
-            f.close()
-        else:
-            f = open(cache_file, "r")
-            data = f.read()
-            f.close()
-    else:
-        print 'ANIMES-BR Provider: No cache!'
-        req = urllib2.Request(url)
-        data = urllib2.urlopen(req).read()
-        f = open(cache_file, "w")
-        f.write(data)
-        f.close()
+def get_shippuden_fix():
+    data = get_cached_func('get_url' ,('http://en.wikipedia.org/wiki/List_of_Naruto:_Shippuden_episodes',))
+    return [{"season": season, "first_episode": first_episode } for season, first_episode in re.findall(r'>(?:Season ([0-9][0-9]*))?:.*?>([0-9]*)<\/th>', data, re.DOTALL)]
+
+def get_url(url):
+    print PREFIX_LOG + 'Downloading ' + url
+    req = urllib2.Request(url, headers=HEADERS)
+    data = urllib2.urlopen(req).read()
     return data
 
-PAYLOAD = json.loads(base64.b64decode(sys.argv[1]))
-urllib2.urlopen(
-PAYLOAD["callback_url"],
-data=json.dumps(globals()[PAYLOAD["method"]](*PAYLOAD["args"]))
-)
-print 'ANIMES-BR Provider Time: ' + str(time.time() - inicio)
+def get_cached_func(funcName,funcParm=(False,)):
+    m = hashlib.md5()
+    m.update(funcName + str(funcParm))
+    key = m.hexdigest()
+    cache_file = cache_prefix + key + '.db'
+    f = globals()[funcName]
+    d = shelve.open(cache_file)
+    if (d.has_key(key)):
+        value = d[key]
+        d.close()
+        thread.start_new_thread(update_cache, (key,funcName,funcParm, ))
+        return value
+    else:
+        if(funcParm[0] == False):
+            d[key] = f()
+        else:
+            d[key] = f(*funcParm)
+        return d[key]
+
+def update_cache(key,funcName,funcParm):
+    m = hashlib.md5()
+    m.update(funcName + str(funcParm))
+    key = m.hexdigest()
+    cache_file = cache_prefix + key + '.db'
+    f = globals()[funcName]
+    d = shelve.open(cache_file)
+    if(funcParm[0] == False):
+        d[key] = f()
+    else:
+        d[key] = f(*funcParm)
+    print PREFIX_LOG + 'Cache key: ' + key + ' updated!'
+    d.close()
+    
+print PREFIX_LOG + 'Time: ' + str((time.time() - inicio))
+provider.register(search, search_movie, search_episode)
